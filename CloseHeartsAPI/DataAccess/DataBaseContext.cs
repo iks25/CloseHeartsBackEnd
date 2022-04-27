14cbe1b704651ff6dfdad6680147b4ad0bc6fd3a
Igor Kozerski <igor.kozerski@gmail.com>
2022-04-27 23:17:35 +0100

    Add project files.

---
+++ CloseHeartsAPI/DataAccess/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using CloseHeartsAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloseHeartsAPI.DataAccess
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
